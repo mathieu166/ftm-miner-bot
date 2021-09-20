d330a85729fce54fd47f3b143d5c6bd4e6c9f085
Mathieu Riendeau <riendeau_mathieu@hotmail.com>
2021-09-20 18:35:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Fantom Miner Bot
+
+- Install NodeJS (https://nodejs.org/en/download/)
+
+- Set your private key as an environment variable, FTM_ACC_1
+ex.: FTM_ACC_1=12341234kjdfl213471283lkjfh
+
+- Set your wallet address key as an environment variable WALLET_ADDRESS
+ex: WALLET_ADDRESS=0x22e3353419084219963a47381a71d1824C5858P9
+
+- run 'npm install'
+
+- run 'npm start [frequency]'
+    - npm start => runs every 30 minutes by default
+    - npm start 60 => runs every 60 minutes
+    - npm start 20 => runs every 20 minutes 
+
+- relax now and go invest in DRIP
+
---
+++ index.js
@@ -0,0 +1,49 @@
+import cron from 'node-cron'
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import fs from 'fs'
+let rawdata = fs.readFileSync(__dirname + '/abi.json');
+let abi = JSON.parse(rawdata);
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+import Web3 from 'web3'
+
+const PRIVATE_KEY = process.env.FTM_ACC_1
+const WALLET_ADDRESS = process.env.WALLET_ADDRESS
+const provider = new Web3.providers.HttpProvider(
+  'https://rpc.ftm.tools'
+)
+
+var args = process.argv.slice(2)
+
+const web3 = await new Web3(provider)
+web3.eth.accounts.wallet.add(PRIVATE_KEY)
+
+const account = web3.eth.accounts.wallet[0].address
+
+const contract = new web3.eth.Contract(abi, '0x69e7D335E8Da617E692d7379e03FEf74ef295899')
+
+console.log('Starting Bot...')
+console.log(`Will buy miners every ${args.length==1?args[0]:'30'} minutes`)
+const executor = async () =>{const estimatedGas = await contract.methods.hatchEggs(WALLET_ADDRESS)
+        .estimateGas(
+          {
+            from: WALLET_ADDRESS,
+            gasPrice: web3.utils.toWei('500', 'gwei')
+          })
+      let result = await contract.methods.hatchEggs(WALLET_ADDRESS).send({ from: account, gasPrice: web3.utils.toWei('250', 'gwei') , gas: estimatedGas });
+      const block = await web3.eth.getBlock(result.blockNumber)
+
+      var d = new Date(0) // The 0 there is the key, which sets the date to the epoch
+      d.setUTCSeconds(block.timestamp)
+
+      console.log(`Successful - ${d.toLocaleString()} - More Miners hired on block ${result.blockNumber}.`)
+  }
+  cron.schedule(`*${args.length==1?'\\'+args[0]:'\\30'} * * * *`, executor)
+  executor()
